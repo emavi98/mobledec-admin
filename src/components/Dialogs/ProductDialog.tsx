8471ff0dc1405b7a07037d63a06e48bded02a84c
Macoding95 <macoding@hotmail.com>
2023-11-10 07:41:28 -0500

    fix: dialog

--- src/components/Dialogs/ProductDialog.tsx
+++ src/components/Dialogs/ProductDialog.tsx
@@ -296,7 +296,7 @@ const ProductDialog = () => {
     }
   }, [dialog]);
   return (
-    <Dialog open={open && dialog} onOpenChange={setOpen}>
+    <Dialog open={open} onOpenChange={setOpen}>
       <DialogTrigger className="inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 bg-primary text-primary-foreground hover:bg-primary/90 h-10 px-4 py-2 w-[200px]">
         Crear Producto
       </DialogTrigger>
